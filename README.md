f2b8cd003aec4accf77bb5618383539bf03c6bd1
Ethan <pugnator.yngvarr@gmail.com>
2021-01-26 09:59:03 -0600

    Added comments and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # My Message Generator
 ---
-This is a project designed to generate random messages using Javascript.
+This is a project designed to generate random messages. This project was intended to practice Javascript syntax, command line, and git/Github version control.
+
+## Technologies
+---
+- Javascript
+- Node.js
+
+## Setup
+---
+To install, download the zip file from github as pictured below, then extract to the desired location.
+![Click the code button on the righthand side, and click download zip](https://github.com/Kyricchi/Mixed-Messages-Project/blob/main/github_download_zip.png?raw=true)
+
+To run the script, navigate to the containing directory in command line and type:
+
+    $ node script.js
--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+//Arrays of subjects and predicates to be pulled from to create random phrase
 let subjects = ['life', 'future', 'happiness', 'heart', 'smile', 'effort', 'compassion', 'curiousity', 'ambition', 'fortune', 'imagination', 'new idea', 'competence', 'talents', 'success', 'quick wits', 'reputation', 'love', 'mind', 'mentality', 'ideals', 'infinite capacity for patience'];
 let predicates = ['is a present you give yourself', 'will ultimately pay off', 'is often more important than a passionate romance', 'is what you need now', 'is the foundation of victory', 'will lead you to success', 'will bring you good luck', 'marks the onset of a friendship', 'is your greatest asset', 'will astonish everyone', 'will be recognized and suitably rewarded', 'can capture the highest status or prestige'];
 
+//function to pull a random string from the above array
 const returnRandomPhrase = arr => {
+    //Creates a random number from 0 to length of the arry to be used as the index of the array
     let selector = Math.floor(Math.random() * arr.length);
     return arr[selector];
 };
 
+//uses the returnRandomPhrase function to pull a subject and predicate and print the message to console
 const randomMessage = () => {
     let randSubject = returnRandomPhrase(subjects);
     let randPredicate = returnRandomPhrase(predicates);
